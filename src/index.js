a9b5af23f1d03b1647442c9e651e0292fe510c96
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-03-14 23:15:58 +0300

    linter configured

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import textColor from "./lib/textColor.js";
+import textColor from './lib/textColor.js';
 
 console.log(textColor.yellow, 'Hello! I`m insdex.js <-----');
 
-import { program } from "commander";
+import { program } from 'commander';
 
 program
-.version('0.1.0')
-.description('Compares two configuration files and shows a difference.')
-.option('-f --format <type>', 'output format')
-.arguments('<filepath1>, <filepath2>')
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f --format <type>', 'output format')
+  .arguments('<filepath1>, <filepath2>');
 
-export default program.parse()
+export default program.parse();
