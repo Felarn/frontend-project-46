c5f31e75fb52fefe1fd4e1758d6b8e46e3409c71
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-03-14 13:19:43 +0300

    bin and script separated

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
-import { program } from "commander";
-
-program
-.version('0.1.0')
-.description('Compares two configuration files and shows a difference.')
-.option('-f --format <type>', 'output format')
-.arguments('<filepath1>, <filepath2>')
-
-program.parse()
-
-// import main from '../src/index.js';
-// main();
+import main from '../src/index.js';
+main;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-export default () => {
-    console.log('Hello!');
-}
+console.log('Hello! I`m insdex.js <-----');
+
+import { program } from "commander";
+
+program
+.version('0.1.0')
+.description('Compares two configuration files and shows a difference.')
+.option('-f --format <type>', 'output format')
+.arguments('<filepath1>, <filepath2>')
+
+export default program.parse()
