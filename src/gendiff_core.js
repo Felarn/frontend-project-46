de9d82e8cc9018770c3a6086a9354e045d8aa848
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-03-16 02:46:24 +0300

    fixed file read

--- src/gendiff_core.js
+++ src/gendiff_core.js
@@ -1,9 +1,10 @@
 import textColor from './lib/textColor.js';
+import getTextFromFile from './lib/getTextFromFile.js';
 import fs from 'fs';
 
 export default function gendiff(path1, path2, options = {}) {
-  const obj1 = JSON.parse(fs.readFileSync(path1));
-  const obj2 = JSON.parse(fs.readFileSync(path2));
+  const obj1 = JSON.parse(getTextFromFile(path1) || '{}');
+  const obj2 = JSON.parse(getTextFromFile(path2) || '{}');
 
   const allEntries = Object.keys({ ...obj1, ...obj2 }).sort();
 
