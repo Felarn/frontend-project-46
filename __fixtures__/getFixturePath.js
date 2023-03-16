4bbc08857f10aa0c829eab2a1dfd23e71d7d6c9e
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-03-17 01:20:52 +0300

    getFixturePath separated to standalone file

---
+++ __fixtures__/getFixturePath.js
@@ -0,0 +1,11 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const fixturesPath = path.resolve(
+  fileURLToPath(import.meta.url),
+  '../../__fixtures__'
+);
+
+const getFixturePath = (...parts) => path.resolve(fixturesPath, ...parts);
+
+export default getFixturePath;
