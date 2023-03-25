c9cee0ed1c8426a7c2b67d143c6a894371dbaef2
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-03-26 01:28:52 +0300

    no color

--- src/gendiff_core.js
+++ src/gendiff_core.js
@@ -1,7 +1,7 @@
 import getObjFromFile from './lib/getObjFromFile.js';
 import getDiff from './lib/getDiff.js';
 import doFormatting from './lib/doFormatting.js';
-import { display, getColorTags } from './lib/visuals.js';
+// import { display, getColorTags } from './lib/visuals.js';
 
 export default function gendiff(path1, path2, formatStyle = 'stylish') {
   const obj1 = getObjFromFile(path1);
@@ -9,8 +9,8 @@ export default function gendiff(path1, path2, formatStyle = 'stylish') {
 
   const diff = getDiff(obj1, obj2);
   const formattedDiff = doFormatting(diff, formatStyle);
-  const colorTags = getColorTags(diff, formatStyle);
-  display(formattedDiff, colorTags);
+  // const colorTags = getColorTags(diff, formatStyle);
+  // display(formattedDiff, colorTags);
 
   console.log(formattedDiff.join('\n'));
   return formattedDiff.join('\n');
