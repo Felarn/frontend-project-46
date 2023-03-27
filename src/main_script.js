ad336495b516955b04f51c0a162d5b88d6981fb1
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-03-27 13:55:53 +0300

    options order changed

--- src/main_script.js
+++ src/main_script.js
@@ -7,9 +7,9 @@ const { version } = JSON.parse(getTextFromFile('package.json'));
 program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
-  .option('-f --format <type>', 'output format')
   .option('-c --colorize', 'colorize terminal output')
-  .arguments('<filepath1>, <filepath2>')
+  .option('-f --format <type>', 'output format')
+  .arguments('filepath1 filepath2')
   .action((filepath1, filepath2, options) => {
     const format = options.format ?? 'stylish';
     if (!['stylish', 'plain', 'json'].includes(format)) {
