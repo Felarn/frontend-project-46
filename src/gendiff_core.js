e7e94a37294323080b1f60b9bd3a33821176f249
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-03-27 11:43:12 +0300

    cleanup code

--- src/gendiff_core.js
+++ src/gendiff_core.js
@@ -5,7 +5,6 @@ import applyFormat from './lib/applyFormat.js';
 export default function gendiff(path1, path2, formatStyle = 'stylish') {
   const obj1 = getObjFromFile(path1);
   const obj2 = getObjFromFile(path2);
-
   const diff = getDiff(obj1, obj2);
   return applyFormat(diff, formatStyle);
 }
